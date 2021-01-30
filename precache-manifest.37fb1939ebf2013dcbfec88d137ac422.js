487945667487c946f2343451dde5f2c2080a56f7
Dimasyoga <dimasyogapra@gmail.com>
2021-01-30 23:57:12 +0700

    Updates

---
+++ precache-manifest.37fb1939ebf2013dcbfec88d137ac422.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9999979870f5c372426e22158614d6f",
+    "url": "/aboutme/index.html"
+  },
+  {
+    "revision": "caa81185259844b58002",
+    "url": "/aboutme/static/css/3.3a26ebde.chunk.css"
+  },
+  {
+    "revision": "bdbfcd50a48235474d51",
+    "url": "/aboutme/static/css/4.21259c32.chunk.css"
+  },
+  {
+    "revision": "74514abe237b043a1311",
+    "url": "/aboutme/static/css/main.4cb10cc0.chunk.css"
+  },
+  {
+    "revision": "5463287fbc6d4760344f",
+    "url": "/aboutme/static/js/2.6ab9df2b.chunk.js"
+  },
+  {
+    "revision": "d0d8190170b812713050bd2e6e947e19",
+    "url": "/aboutme/static/js/2.6ab9df2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caa81185259844b58002",
+    "url": "/aboutme/static/js/3.ad7de46b.chunk.js"
+  },
+  {
+    "revision": "bdbfcd50a48235474d51",
+    "url": "/aboutme/static/js/4.5d8554bf.chunk.js"
+  },
+  {
+    "revision": "74514abe237b043a1311",
+    "url": "/aboutme/static/js/main.5a143b99.chunk.js"
+  },
+  {
+    "revision": "af26dfdcc87e4ca3b248",
+    "url": "/aboutme/static/js/runtime-main.07ac2bbe.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/aboutme/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/aboutme/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/aboutme/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/aboutme/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "c1a95f7335cbfb730b937e19e8830a53",
+    "url": "/aboutme/static/media/programmer.c1a95f73.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/aboutme/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "39eff4a91b01e00a32cb78f3797eec88",
+    "url": "/aboutme/static/media/talksCardBack.39eff4a9.svg"
+  }
+]);
