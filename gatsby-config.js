7cf97a204094b62598db330bb53b37fb414e501a
Dimasyoga <dimasyogapra@gmail.com>
2021-05-13 10:18:29 +0700

    [REVERT] to 5b8abea843deb9bed79a86a8e32551ce656a2312

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,44 +1,39 @@
 module.exports = {
   pathPrefix: "/aboutme",
   siteMetadata: {
-    title: `Portfolio`,
-    description: `My Portfolio`,
-    author: `dim`,
+    title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
     siteUrl: 'https://dimasyoga.github.io/aboutme/',
   },
-  flags: { PRESERVE_WEBPACK_CACHE: true },
   plugins: [
-    `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    'gatsby-plugin-robots-txt',
+    `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images/`,
+        path: `${__dirname}/src/images`,
       },
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `pages`,
-        path: `${__dirname}/src/pages/`,
-      },
-    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Simplefolio`,
-        short_name: `Simplefolio`,
-        start_url: `/aboutme`,
-        background_color: `#fff`,
-        theme_color: `#02aab0`,
-        display: `standalone`,
-        icon: 'src/images/favicon.png',
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-offline`,
+    `gatsby-plugin-gatsby-cloud`,
+    'gatsby-plugin-robots-txt',
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
   ],
-};
+}
